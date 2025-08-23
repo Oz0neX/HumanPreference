c82c592d35b81a9a1e0d543e7cd8688a1aa07fd0
Mr. White <y.white005@gmail.com>
2025-08-22 19:26:40 -0700

    Updated for KL divergence

--- train.py
+++ train.py
@@ -6,79 +6,113 @@ import numpy as np
 from metadrive.envs.metadrive_env import MetaDriveEnv
 from stable_baselines3.common.policies import ActorCriticPolicy
 import torch
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-import csv
+from scipy.stats import entropy
+
+BATCH_SIZE = 300
+TRAINING_EPOCHS = 300
 
 maps_data = extract_data()
-
 if not maps_data:
-    print("No trajectory data was found. Exiting.")
     sys.exit(1)
 
-def train(transitions, seed, demo_type):
-    env = MetaDriveEnv(config={
-        "on_continuous_line_done": False,
-        "out_of_route_done": False,
-        "crash_vehicle_done": False,
-        "crash_object_done": False,
-        "use_render": False,
-        "manual_control": False,
-        "traffic_density": 0,
-        "map": "SCS",
-        "start_seed": seed,
-        "horizon": 1000
-    })
+map_name = list(maps_data.keys())[0]
+trajectories_dict = maps_data[map_name]
+seed = int(map_name.split('_')[1])
 
-    custom_policy = ActorCriticPolicy(
-        observation_space=env.observation_space,
-        action_space=env.action_space,
-        net_arch=[32, 64, 32, 6],
-        lr_schedule=lambda _: 3e-4
-    )
-    
-    bc_trainer = bc.BC(
-        observation_space=env.observation_space,
-        action_space=env.action_space,
-        demonstrations=transitions,
-        rng=np.random.default_rng(0),
-        policy=custom_policy,
-        batch_size=300,
+optimal_transitions = flatten_trajectories(trajectories_dict.get('optimal', []))
+teaching_transitions = flatten_trajectories(trajectories_dict.get('teaching', []))
+
+env = MetaDriveEnv(config={
+    "on_continuous_line_done": False,
+    "out_of_route_done": False,
+    "crash_vehicle_done": False,
+    "crash_object_done": False,
+    "use_render": False,
+    "manual_control": False,
+    "traffic_density": 0,
+    "map": "SCS",
+    "start_seed": seed,
+    "horizon": 1000
+})
+
+def create_policy():
+    return ActorCriticPolicy(
+        observation_space=env.observation_space, action_space=env.action_space,
+        net_arch=[32, 64, 32, 6], lr_schedule=lambda _: 3e-4
     )
 
-    bc_trainer.train(n_epochs=300)
-    by_step, by_traj, by_step_random, by_traj_random = bc_trainer.calculate_teaching_volume()
-    
-    all_traj_ids = sorted(by_step.keys())
-    
-    with open(f'teaching_volume_{demo_type}_results.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['trajectory_id', 'bc_timestep', 'bc_trajectory', 'random_timestep', 'random_trajectory'])
-        for t_id in all_traj_ids:
-            writer.writerow([
-                t_id,
-                round(by_step.get(t_id, 0), 5),
-                round(by_traj.get(t_id, 0), 5),
-                round(by_step_random.get(t_id, 0), 5),
-                round(by_traj_random.get(t_id, 0), 5)
-            ])
-        
-        writer.writerow([
-            'SUM',
-            round(sum(by_step.values()), 5),
-            round(sum(by_traj.values()), 5),
-            round(sum(by_step_random.values()), 5),
-            round(sum(by_traj_random.values()), 5)
-        ])
-    
-for map_name, trajectories_dict in maps_data.items():
-    seed = int(map_name.split('_')[1])
-    
-    optimal_trajectories = trajectories_dict.get('optimal', [])
-    optimal_transitions = flatten_trajectories(optimal_trajectories)
-    train(optimal_transitions, seed, "optimal")
-    
-    teaching_trajectories = trajectories_dict.get('teaching', [])
-    teaching_transitions = flatten_trajectories(teaching_trajectories)
-    train(teaching_transitions, seed, "teaching")
+def calculate_distribution(bc_tv_dict, random_tv_dict):
+    bc_tvs = np.array(list(bc_tv_dict.values()))
+    rand_tvs = np.array(list(random_tv_dict.values()))
+    sum_exp_bc = np.sum(np.exp(bc_tvs))
+    sum_exp_rand = np.sum(np.exp(rand_tvs))
+    denominator = sum_exp_bc + sum_exp_rand
+    raw_likelihoods = np.exp(bc_tvs) / denominator
+    final_dist = raw_likelihoods / np.sum(raw_likelihoods)
+    return final_dist
+
+optimal_model = bc.BC(
+    observation_space=env.observation_space,
+    action_space=env.action_space,
+    demonstrations=optimal_transitions,
+    rng=np.random.default_rng(0),
+    policy=create_policy(),
+    batch_size=BATCH_SIZE
+)
+eval_grad_norm_opt = optimal_model.train(n_epochs=TRAINING_EPOCHS)
+
+teaching_model = bc.BC(
+    observation_space=env.observation_space,
+    action_space=env.action_space,
+    demonstrations=teaching_transitions,
+    rng=np.random.default_rng(0),
+    policy=create_policy(),
+    batch_size=BATCH_SIZE
+)
+eval_grad_norm_teach = teaching_model.train(n_epochs=TRAINING_EPOCHS)
+
+optimal_demos = optimal_model._demo_data_loader
+teaching_demos = teaching_model._demo_data_loader
+
+tv_opt_on_opt = optimal_model.calculate_teaching_volume(eval_grad_norm_opt, optimal_demos)
+tv_opt_on_teach = optimal_model.calculate_teaching_volume(eval_grad_norm_opt, optimal_demos)
+
+tv_teach_on_teach = teaching_model.calculate_teaching_volume(eval_grad_norm_teach, teaching_demos)
+tv_teach_on_opt = teaching_model.calculate_teaching_volume(eval_grad_norm_teach, teaching_demos)
+
+results = {}
+
+# Data format tv_type_on_type[0, 1, 2, 3]
+# tv_type_on_type[0]: BC Timestep TV
+# tv_type_on_type[1]: BC Trajectory TV
+# tv_type_on_type[2]: Random Timestep TV
+# tv_type_on_type[3]: Random Trajectory TV
+
+# By timestep
+dist_opt_on_opt_ts = calculate_distribution(tv_opt_on_opt[0], tv_opt_on_opt[2])
+dist_opt_on_teach_ts = calculate_distribution(tv_opt_on_teach[0], tv_opt_on_teach[2])
+results['kl_opt_model_timestep'] = entropy(dist_opt_on_opt_ts, dist_opt_on_teach_ts)
+
+dist_teach_on_teach_ts = calculate_distribution(tv_teach_on_teach[0], tv_teach_on_teach[2])
+dist_teach_on_opt_ts = calculate_distribution(tv_teach_on_opt[0], tv_teach_on_opt[2])
+results['kl_teach_model_timestep'] = entropy(dist_teach_on_teach_ts, dist_teach_on_opt_ts)
+
+# By trajectory
+dist_opt_on_opt_tj = calculate_distribution(tv_opt_on_opt[1], tv_opt_on_opt[3])
+dist_opt_on_teach_tj = calculate_distribution(tv_opt_on_teach[1], tv_opt_on_teach[3])
+results['kl_opt_model_trajectory'] = entropy(dist_opt_on_opt_tj, dist_opt_on_teach_tj)
+
+dist_teach_on_teach_tj = calculate_distribution(tv_teach_on_teach[1], tv_teach_on_teach[3])
+dist_teach_on_opt_tj = calculate_distribution(tv_teach_on_opt[1], tv_teach_on_opt[3])
+results['kl_teach_model_trajectory'] = entropy(dist_teach_on_teach_tj, dist_teach_on_opt_tj)
+
+print("\n" + "="*50)
+print(" " * 15 + "KL DIVERGENCE RESULTS")
+print("="*50)
+print("\n## Timestep Divergence ##")
+print(f"Optimal Model KL: {results['kl_opt_model_timestep']:.5f}")
+print(f"Teaching Model KL: {results['kl_teach_model_timestep']:.5f}")
+print("\n## Trajectory Divergence ##")
+print(f"Optimal Model KL: {results['kl_opt_model_trajectory']:.5f}")
+print(f"Teaching Model KL: {results['kl_teach_model_trajectory']:.5f}")
+print("\n" + "="*50)
