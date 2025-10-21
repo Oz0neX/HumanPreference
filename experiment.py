64091043aff0a0fc38a6fbcc2867719ad300de3b
Mr. White <y.white005@gmail.com>
2025-10-20 18:54:22 -0700

    Revert

--- experiment.py
+++ experiment.py
@@ -114,9 +114,9 @@ class RobotTeachingApp:
         optimal_sequence = ["human_demo"] * NUM_OPTIMAL
 
         # Generate teaching phase sequence: agent-human alternating for NUM_TEACHING cycles
-        teaching_sequence = ["noisy_expert"]
+        teaching_sequence = []
         for i in range(NUM_TEACHING):
-            teaching_sequence.extend(["human"])
+            teaching_sequence.extend(["noisy_expert", "human"])
 
         # Combine: optimal first, then teaching (same order regardless of experiment type)
         self.phase_sequence = optimal_sequence + teaching_sequence
